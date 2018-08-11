0d2ecfe26d1602fac196008f0566f9eb4c2eaca0
Den Williams <work@denwilliams.net>
2018-08-11 16:25:35 +1000

    light and bridge types

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const { MilightController, commands, commands2 } = require('node-milight-promise');
+const milight = require('node-milight-promise');
+const { MilightController } = milight;
 
 const mqttusvc = require('mqtt-usvc');
 const service = mqttusvc.create();
@@ -16,10 +17,17 @@ service.on('message', (topic, data) => {
   const device = lights[light];
   if (!device) return;
 
-  if (value === 'on') {
-    device.on();
-  } else if (value === 'off') {
-    device.off();
+  switch (value) {
+    case 'on':
+      device.on();
+      break;
+    case 'off':
+      device.off();
+      break;
+    case 'raw':
+      if (typeof data === 'string') device.raw([data]);
+      else device.raw(data);
+      break;
   }
 });
 
@@ -27,28 +35,43 @@ service.config.bridges.forEach(b => {
   bridges[b.id] = new MilightController({
     ip: b.host,
     delayBetweenCommands: 75,
-    commandRepeat: 2
+    commandRepeat: 2,
+    type: b.type
   });
+  const cmdStr = b.commands || 'commandsV6';
+  bridges[b.id].commands = milight[cmdStr];
 });
 
 service.config.lights.forEach(l => {
   const bridge = bridges[l.bridge];
   const zone = l.zone;
+  const type = l.type || 'white';
+  const cmd = bridge.commands[type];
+
   lights[l.id] = {
     bridge,
     zone,
     on() {
       console.log(`Turning on ${l.id}`);
       // bridge.sendCommands(commands.white.on(zone), commands.white.maxBright(zone));
-      bridge.sendCommands(commands.white.on(zone));
+      bridge.sendCommands(cmd.on(zone));
       service.send(`status/${l.id}`, {on: true});
     },
     off() {
       console.log(`Turning off ${l.id}`);
-      bridge.sendCommands(commands.white.off(zone));
+      bridge.sendCommands(cmd.off(zone));
       service.send(`status/${l.id}`, {on: false});
+    },
+    raw(actions) {
+      const cmdActions = actions.map(action => {
+        const fn = cmd[action];
+        return fn(zone);
+      });
+      console.log(`Raw ${l.id} ${actions}`);
+      bridge.sendCommands(...cmdActions);
     }
   };
   service.subscribe('set/' + l.id + '/on');
   service.subscribe('set/' + l.id + '/off');
+  service.subscribe('set/' + l.id + '/raw');
 });
